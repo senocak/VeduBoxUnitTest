9090a3f9478f29d0ed3a4f8faee77142ce30354c
Anıl Şenocak <asenocak@netas.com.tr>
2021-01-05 12:20:37 +0300

    initial commit

--- VeduBoxUnitTest/Kurumsal/Pages/CoursesPage.cs
+++ VeduBoxUnitTest/Kurumsal/Pages/CoursesPage.cs
@@ -3,7 +3,6 @@ using System;
 using System.IO;
 using System.Text;
 using VeduBoxUnitTest.Assertion;
-using OpenQA.Selenium.Support.UI;
 
 namespace VeduBoxUnitTest.Kurumsal.Pages{
     class CoursesPage : Page{
